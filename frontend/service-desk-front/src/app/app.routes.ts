d7d46efefadf458b99d63d40b499a11253c716c0
joelap16 <mcpejoel@gmail.com>
2026-06-14 18:25:07 -0500

    Login y Register components

--- frontend/service-desk-front/src/app/app.routes.ts
+++ frontend/service-desk-front/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/auth/login/login';
+import { Register } from './pages/auth/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'auth/login', pathMatch: 'full' },
+  { path: 'auth/login', component: Login },
+  { path: 'auth/register', component: Register },
+];
