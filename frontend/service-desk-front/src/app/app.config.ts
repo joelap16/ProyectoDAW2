795a0c34be1909da79b5ec2614564a14ec2d8580
joelap16 <mcpejoel@gmail.com>
2026-06-20 01:03:57 -0500

    JWT Interceptor

--- frontend/service-desk-front/src/app/app.config.ts
+++ frontend/service-desk-front/src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { jwtInterceptor } from './interceptors/jwt-interceptor';
+
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,7 +9,11 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(),
+    
+    provideHttpClient(
+      withInterceptors([jwtInterceptor])
+    ),
+
     provideRouter(routes)
   ]
 };
---
+++ frontend/service-desk-front/src/app/interceptors/jwt-interceptor.ts
@@ -0,0 +1,26 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+
+import { AuthService } from '../services/auth/auth.service';
+
+export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const authService = inject(AuthService);
+
+  const token = authService.obtenerToken();
+
+  if (token) {
+
+    req = req.clone({
+
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+
+    });
+
+  }
+
+  return next(req);
+
+};
